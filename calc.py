d7d91e5f7fc523363b36dc8de4a635e8f9fbb2ba
Aditya Shinde <aditya.shinde@example.com>
2025-03-19 18:08:56 +0530

    test

--- calc.py
+++ calc.py
@@ -18,7 +18,7 @@ def power(x, b):
 
 def main():
     while True:
-        print("\nScientific Calculator")
+        print("\nScientific Calculator Menu")
         print("1. Square Root (√x)")
         print("2. Factorial (x!)")
         print("3. Natural Logarithm (ln(x))")
